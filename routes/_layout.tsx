04e7aa1d86387b1749e10b3d39247b410cb2ff64
describanoa <describanoa@alumnos.nebrija.es>
2025-04-04 12:22:26 +0200

    commit 6 - examen

--- routes/_layout.tsx
+++ routes/_layout.tsx
@@ -3,7 +3,6 @@ import { Footer } from "../components/footer.tsx";
 import { Header } from "../components/Header.tsx";
 
 export default function Layout({ Component }: PageProps) {
-  // do something with state here
     return (
         <div class="layout">
             <Header />
