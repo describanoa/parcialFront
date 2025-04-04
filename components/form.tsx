460d0cbf1244b669218aa6abca5365f34386c3e2
describanoa <describanoa@alumnos.nebrija.es>
2025-04-04 12:15:33 +0200

    commit 4 - examen

--- components/form.tsx
+++ components/form.tsx
@@ -2,7 +2,7 @@ import { FunctionalComponent } from "preact/src/index.d.ts";
 
 export const Form: FunctionalComponent = () => {
     return(
-        <div class="formBuscarTelefono">
+        <div>
             <form action="/" method="get">
                 <input type="text" name="phone" placeholder="Buscar..."/>
                 <button type="submit">Buscar</button>
